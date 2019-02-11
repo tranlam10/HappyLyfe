0a77bceab7f1f7c53ef1e8fcfb7c9ed179ff5d3d
Lam Tran <lam.tran10@gmail.com>
2019-02-11 13:19:36 -0800

    changed test

--- test/test-status200.js
+++ test/test-status200.js
@@ -3,12 +3,15 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = require('chai').expect;
+const app = require('../server');
 chai.use(chaiHttp);
 
 describe('status 200 works', function() {
     it('should return status of 200', function() {
-        return chai.request('http://localhost:8080', function (){
-            expect(res).to.have.status(200);
-        });
+        return chai.request(app)
+            .get("/")
+            .then(function() {
+                expect(res).to.have.status(200);
+            });
     });
 });
